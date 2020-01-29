6312a33f096ea3226ed0a190c25599d185982a2c
Shilpa Chelagamsetti <1681523@TCSNSN.COM>
2020-01-29 15:59:39 +0530

    pushing latest code

--- src/app/home/project-details/project-details.component.ts
+++ src/app/home/project-details/project-details.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup}  from '@angular/forms';
+import { FormBuilder} from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-project-details',
@@ -7,26 +13,70 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./project-details.component.scss']
 })
 export class ProjectDetailsComponent implements OnInit {
-  projectDetailsArray: any;
+  projectDetailsArray: any = [
+    {  pname:"p1", pstatus:"Completed", pduration:"3 months", ptechnology:"Angular", ptype:"SoftwareProject", pdesc:"The name of the hjjs jsadf dfhj"},
+    {  pname:"p2", pstatus:"Completed", pduration:"6 months", ptechnology:"Node",  ptype:"SoftwareProject", pdesc:"The name of the hjjs jsadf dfhj"},
+    {  pname:"p3", pstatus:"InProgress", pduration:"9 months", ptechnology:"Python",  ptype:"SoftwareProject", pdesc:"The name of the hjjs jsadf dfhj" },
+    {  pname:"p4", pstatus:"InProgress", pduration:"12 months", ptechnology:"Java", ptype:"SoftwareProject", pdesc:"The name of the hjjs jsadf dfhj" }
+  ];
   prj:any
-  constructor(private router:Router,private route : ActivatedRoute) { }
+  editForm: FormGroup;
+  showSavebtn: boolean;
+  constructor(private router:Router,private route : ActivatedRoute, private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.prj = this.route.snapshot.paramMap.get('id');
 
-    this.projectDetailsArray = [
-      {  pname:"p1", pstatus:"Completed", pduration:"3 months", ptechnology:"Angular", pdetails:"SoftwareProject"},
-      {  pname:"p2", pstatus:"Completed", pduration:"6 months", ptechnology:"Node",  pdetails:"SoftwareProject"},
-      {  pname:"p3", pstatus:"InProgress", pduration:"9 months", ptechnology:"Python",  pdetails:"SoftwareProject" },
-      {  pname:"p4", pstatus:"InProgress", pduration:"12 months", ptechnology:"Java", pdetails:"SoftwareProject" }
-    ];
-  }
-  navigateToProject(pname) {
-    this.router.navigate([`/mainpage/projects`]);
-
-    this.projectDetailsArray.forEach(ele => {
-    })
+    this.editForm = this.fb.group({
+      duration: ['', [Validators.required, Validators.maxLength(25), Validators.minLength(3), spaceValidator]],
+      description: ['', [Validators.required, Validators.minLength(4), spaceValidator]],
+    });
    
+
+    this.prj = this.route.snapshot.paramMap.get('id');
+    this.projectDetailsArray = this.projectDetailsArray.filter(ele => {
+      console.log('ele.pname === this.prj',ele.pname === this.prj);
+      if(ele.pname === this.prj){
+        return ele;
+      }
+
+    }
+    );   
+    console.log(' this.projectDetailsArray', this.projectDetailsArray)
   }
 
+  // navigateToProject(pname) {
+  //   this.router.navigate([`/mainpage/projects`]);
+  //   this.projectDetailsArray.forEach(ele => {
+  //   })   
+  // }
+
+  get f() { return this.editForm.controls;}  
+
+  editProject(pduration, pdesc , index){
+    this.showSavebtn = true;
+    this.editForm.setValue({ duration: pduration, description: pdesc });
+  }
+
+  saveProject(i){
+    this.showSavebtn = false;
+    this.projectDetailsArray[i].pduration = this.editForm.value.duration;
+    this.projectDetailsArray[i].pdesc = this.editForm.value.description;
+  }
+
+  cancelProject(index) {
+    this.showSavebtn = false;
+  }
+  deleteProject() {
+
+  }
+
+}
+
+function spaceValidator(control: AbstractControl): { [key: string]: any } | null {
+  const targetValue = control.value;
+  // .replace(/  +/g, ' ')
+  if (targetValue.trim() === '') {
+    return { validInput: true };
+  }
+  return null;
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Validators } from '@angular/forms';
 import { LocationStrategy } from '@angular/common';
 
+declare var $: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,26 +12,51 @@ import { LocationStrategy } from '@angular/common';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  
-  constructor(private route: Router,private fb:FormBuilder) {
+  showMore: boolean;
+
+  constructor(private route: Router, private fb: FormBuilder) {
 
   }
-  get f(){
+  get f() {
     return this.loginForm.controls;
   }
 
   onSubmit() {
-    console.log("onSubmit",this.loginForm.value);
-    this.route.navigate(['/mainpage']);  
+    console.log("onSubmit", this.loginForm.value);
+    this.route.navigate(['/mainpage']);
   }
 
   ngOnInit() {
 
-this.loginForm =  this.fb.group({
- email: ['', [Validators.required, Validators.minLength(3)]],
- pwd: ['']
-});
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.minLength(3)]],
+      pwd: ['']
+    });
 
+    this.showMore = false;
+
+  } //ngOnInit
+
+  @HostListener('window:scroll', ['$event'])
+  scrollHandler(event) {
+    $(window).scroll(function () {
+      // let position = $(document).height() - ($(window).scrollTop() + $(window).height())
+
+      // console.log("Scroll down - 1");
+      // document.getElementById('pqw').style.visibility = "hidden";
+
+
+      if ($(window).scrollTop() <= ($(document).height() - $(window).height())) {
+        console.log("At last Before", this.showMore);
+        document.getElementById('pqw').style.visibility = "visible";
+        document.getElementById('pqw').style.animation = "fadeIn 5s";
+        // this.showMore = true;
+        // console.log("At last", this.showMore);
+      }
+
+    });
   }
+
+
 
 }
